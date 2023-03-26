53d5d00d57487576f63c5a914ac1767ed465ad6a
walkerxian <cvchina@yahoo.com>
2023-03-27 00:38:15 +0800

    Job Execute()方法本质
    总的来说 主要还是将C# 中的Job 数据复制到 Native 层，最终由Native job System去执行 job中的逻辑。
    
    Execute()方法中修改的其实是 Counter job的一个副本，也就是每个子线程都是得到一个Copy Data，然后再自己的Execute方法中执行
    
    如果需要从子线程中获取job里面计算的数据，需要使用NativeContainer

---
+++ Assets/JobSystem/CounterTest.cs
@@ -0,0 +1,68 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+    /// <summary>
+    /// 计算一个数组里面所有数值的累加器
+    /// </summary>
+public class CounterTest : MonoBehaviour
+{
+    public struct CounterJob : IJob
+    {
+        public NativeArray<int> numbers;
+        //public int result;
+        public NativeArray<int> result;
+
+        public void Execute()
+        {
+            //throw new System.NotImplementedException();
+
+            int temp = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                temp += numbers[i];
+            }
+            result[0] = temp;
+        }
+    };
+    
+    static int numCount = 10;
+    //声明NativeContainer数据
+    NativeArray<int> numbers = new NativeArray<int>(numCount, Allocator.TempJob);
+    NativeArray<int> result = new NativeArray<int>(1, Allocator.TempJob);
+    private JobHandle handle;
+
+    void Start()
+    {
+        //初始化数据
+        for (int i = 0; i < numCount; i++)
+        {
+            numbers[i] = i + 1;
+        }
+
+        //装载数据
+        var jobData = new CounterJob()
+        {
+            numbers = numbers,
+            result = result
+        };
+
+        handle = jobData.Schedule();
+        
+    }
+
+    void Update()
+    {
+        if (handle.IsCompleted)
+        {
+            handle.Complete();
+            Debug.Log($"The result = {result[0]} ");
+        }
+    }
+    
+    public void OnDestroy()
+    {
+        result.Dispose();
+        numbers.Dispose();
+    }
+}
